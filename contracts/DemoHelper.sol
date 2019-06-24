16b67b3cc6c27a0a059487257de2011154435b40
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2019-06-24 13:53:42 +0530

    HydroPay v1.02

--- contracts/DemoHelper.sol
+++ contracts/DemoHelper.sol
@@ -5,6 +5,7 @@ import "./interfaces/SnowflakeInterface.sol";
 import "./interfaces/IdentityRegistryInterface.sol";
 
 import "./zeppelin/ownership/Ownable.sol";
+import "./zeppelin/math/SafeMath.sol";
 
 // @deprecated
 /*
@@ -14,20 +15,34 @@ contract HydroRinkeby {
 */
 
 contract DemoHelper is Ownable {
+    using SafeMath for uint;
+    
     address public snowflakeAddress;
     SnowflakeInterface private snowflake;
     IdentityRegistryInterface private identityRegistry;
     address public snowMoResolverAddress;
     
-    address public tokenHolder;
-
-    constructor (address _snowflakeAddress, address _resolverAddress, address _tokenHolder) public {
+    address public hydroTokenAddress;
+    uint internal _regUsers;
+    
+    uint constant atomicUnit = 10**18;
+    
+    uint constant milestoneUno = 2000;
+    uint constant rewardUno = 10000 * atomicUnit;
+    
+    uint constant milestoneDuo = milestoneUno + 3000;
+    uint constant rewardDuo = 5000 * atomicUnit;
+    
+    uint constant milestoneTrio = milestoneDuo + 5000;
+    uint constant rewardTrio = 1000 * atomicUnit;
+    
+    constructor (address _snowflakeAddress, address _resolverAddress, address _hydroTokenAddress) public {
         snowflakeAddress = _snowflakeAddress;
         snowflake = SnowflakeInterface(snowflakeAddress);
         identityRegistry = IdentityRegistryInterface(snowflake.identityRegistryAddress());
         snowMoResolverAddress = _resolverAddress;
         
-        tokenHolder = _tokenHolder;
+        hydroTokenAddress = _hydroTokenAddress;
     }
 
     // wrap createIdentityDelegated and initialize the client raindrop resolver
@@ -54,19 +69,51 @@ contract DemoHelper is Ownable {
         // HydroRinkeby(snowflake.hydroTokenAddress()).getMoreTokens();
 
         // deposit new tokens into snowflake
-        HydroInterface(snowflake.hydroTokenAddress())
-            .approveAndCall(snowflakeAddress, 10000000000000000000000, abi.encode(_ein));
-
+        // HydroInterface(snowflake.hydroTokenAddress())
+        //     .approveAndCall(snowflakeAddress, 10000000000000000000000, abi.encode(_ein));
+        
+        // Increment users and Apply Reward Logic
+        _regUsers.add(1);
+        
+        // Calculate reward
+        uint rewards;
+        if (_regUsers <= milestoneUno) {
+            rewards = rewardUno;
+        }
+        else if (_regUsers <= milestoneDuo) {
+            rewards = rewardDuo;
+        }
+        else if (_regUsers <= milestoneTrio) {
+            rewards = rewardTrio;
+        }
+        
+        // Give reward to snowflake id if reward greater than 0
+        if (rewards > 0) {
+            HydroInterface(snowflake.hydroTokenAddress())
+                .approveAndCall(snowflakeAddress, rewards, abi.encode(_ein));
+        }
+        
         return _ein;
     }
     
-    function setTokenHolder(address _newTokenholder) public onlyOwner {
-        tokenHolder = _newTokenholder;
+    function getRegisteredUsers() public view returns (uint registeredUsers) {
+        registeredUsers = _regUsers;
+    }
+    
+    function sethydroTokenAddress(address _newhydroTokenAddress) public onlyOwner {
+        hydroTokenAddress = _newhydroTokenAddress;
     }
     
     function withdraw(uint amount) public onlyOwner returns(bool) {
         require(amount <= address(this).balance);
         msg.sender.transfer(amount);
+        
+        return true;
+    }
+    
+    function withdrawToken(uint amount) public onlyOwner returns (bool) {
+        HydroInterface hydro = HydroInterface(hydroTokenAddress);
+        hydro.transfer(msg.sender, amount);
         
         return true;
     }
